b0b0fe6f2684f9083e9add2020b08e5604d44762
ryanp54 <ryanp54@yahoo.com>
2019-09-23 22:22:44 -0500

    test: Add merge function for potential future use.

--- app/src/dateUtilities.js
+++ app/src/dateUtilities.js
@@ -34,3 +34,22 @@ export function parseToUTC(iso) {
 	const date = new Date(iso);
 	return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()));
 }
+
+// Simple deep merge solution from user y.c. on stackoverflow: https://stackoverflow.com/a/50228121
+export function merge(current, update) {
+  Object.keys(update).forEach(function(key) {
+    // if update[key] exist, and it's not a string or array,
+    // we go in one level deeper
+    if (current.hasOwnProperty(key) 
+        && typeof current[key] === 'object'
+        && !(current[key] instanceof Array)) {
+      merge(current[key], update[key]);
+
+    // if update[key] doesn't exist in current, or it's a string
+    // or array, then assign/overwrite current[key] to update[key]
+    } else {
+      current[key] = update[key];
+    }
+  });
+  return current;
+}
