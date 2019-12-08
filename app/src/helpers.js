d7899c197ada1e992f32ea37beb685e1dc4e3c6e
ryanp54 <ryanp54@yahoo.com>
2019-12-08 15:20:28 -0600

    refactor: remove unused helper function

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -59,26 +59,3 @@ export function parseToUTC(iso) {
 
   return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()));
 }
-
-// Simple deep merge solution from user y.c. on stackoverflow: https://stackoverflow.com/a/50228121
-// TODO: not used? remove?
-export function merge(current, update) {
-  Object.keys(update).forEach((key) => {
-    // if update[key] exist, and it's not a string or array,
-    // we go in one level deeper
-    if (
-      Object.prototype.hasOwnProperty.call(current, key)
-      && typeof current[key] === 'object'
-      && !(current[key] instanceof Array)
-    ) {
-      merge(current[key], update[key]);
-
-    // if update[key] doesn't exist in current, or it's a string
-    // or array, then assign/overwrite current[key] to update[key]
-    } else {
-      current[key] = update[key];
-    }
-  });
-
-  return current;
-}
