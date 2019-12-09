54998053337ae9f0175c737ba965a3aec37f4479
ryanp54 <ryanp54@yahoo.com>
2019-12-09 00:38:35 -0600

    chore: make build for deployment

---
+++ app/build/precache-manifest.cc88b9c312e6aa0582b5957ae1f475a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e147b50a1a8eb86ae5be9a47f7a284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b48af195fd3522abc8",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "b954381b02403b7feafe",
+    "url": "/static/css/main.d9588103.chunk.css"
+  },
+  {
+    "revision": "06b48af195fd3522abc8",
+    "url": "/static/js/2.13553c93.chunk.js"
+  },
+  {
+    "revision": "b954381b02403b7feafe",
+    "url": "/static/js/main.1b423dcf.chunk.js"
+  },
+  {
+    "revision": "57087445b8c3d9b59894",
+    "url": "/static/js/runtime~main.c2f40307.js"
+  }
+]);
